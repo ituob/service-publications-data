fd4c02dec2d987ea4488a3906405be14aed3c0be
Ronald Tse <ronald.tse@ribose.com>
2020-04-14 16:50:36 +0800

    Cleanup CSV2YAML conversion script

--- scripts/structured_csv_to_yaml.rb
+++ scripts/structured_csv_to_yaml.rb
@@ -13,7 +13,7 @@ module CsvToStructuredHash
 
     csv.each_with_index do |row, index|
       if first_row.nil? && is_start_of_portion?(row, section_name)
-        puts "found first"
+        # puts"found first"
 
         row[1].split(';').each do |opt|
           k, v = opt.split('=')
@@ -25,14 +25,19 @@ module CsvToStructuredHash
       end
 
       if !first_row.nil? && is_row_empty?(row)
-        puts "found last"
+        # puts "found last"
         last_row = index
         break
       end
     end
 
-    puts "first #{first_row}  last #{last_row}"
-    [csv[first_row..last_row], data_meta]
+    # puts "first #{first_row}  last #{last_row}"
+    {
+      first_row: first_row,
+      last_row: last_row,
+      rows: csv[first_row..last_row],
+      meta: data_meta
+    }
   end
 
   def self.is_start_of_portion?(row, section_name)
@@ -55,7 +60,7 @@ module CsvToStructuredHash
     field_name = ""
     field_type = CAST_DEFAULT_TYPE
 
-    puts header_field
+    # puts header_field
     arr = header_field.match(/\A(.*)\[(.*)\]\Z/)
 
     if arr.nil?
@@ -120,11 +125,17 @@ module CsvToStructuredHash
   end
 
   def self.parse_data(rows, data_meta)
-    hash = {}
     header = []
     data_name = data_meta[:name]
     data_type = data_meta[:type] || "hash"
     data_key = data_meta[:key]
+
+    base_structure = case data_type
+    when "hash"
+      {}
+    when "array"
+      []
+    end
 
     rows.each_with_index do |row,index|
       # Assume the first column is always the key
@@ -140,7 +151,7 @@ module CsvToStructuredHash
 
         next
       end
-      puts "header #{header.inspect}"
+      # puts "header #{header.inspect}"
 
       # Skip all the empty rows
       next if is_row_empty?(row)
@@ -168,30 +179,39 @@ module CsvToStructuredHash
         d.delete(k) if d[k].nil?
       end
 
-      hash[k] = normalize_namespaces(d)
+      case data_type
+      when "hash"
+        unless base_structure[k].nil?
+          puts "[WARNING] there is already data inside key [#{k}] -- maybe you should set type=array?"
+        end
+        base_structure[k] = normalize_namespaces(d)
+      when "array"
+        base_structure << normalize_namespaces(d)
+      end
     end
 
     if data_name
-      hash = {
-        data_name => hash
+      base_structure = {
+        data_name => base_structure
       }
     end
 
-    normalize_namespaces(hash)
+    base_structure
   end
 
   def self.convert(csv_filename)
     raw_data = get_csv(csv_filename)
-    metadata, = get_portion(raw_data, "METADATA")
-    data, data_meta = get_portion(raw_data, "DATA")
 
-    puts '----------'
-    pp data
-    puts '----------'
+    metadata_section = get_portion(raw_data, "METADATA")
+    data_section = get_portion(raw_data, "DATA")
+
+    # puts '----------'
+    # pp data_section[:rows]
+    # puts '----------'
 
     {
-      "metadata" => parse_metadata(metadata),
-      "data" => parse_data(data, data_meta)
+      "metadata" => parse_metadata(metadata_section[:rows]),
+      "data" => parse_data(data_section[:rows], data_section[:meta])
     }
   end
 
@@ -203,13 +223,13 @@ module CsvToStructuredHash
     new_hash = {}
 
     hash.each_pair do |k, v|
-      puts "k (#{k}) v (#{v})"
+      # puts"k (#{k}) v (#{v})"
       key_components = k.to_s.split('.')
 
       level = new_hash
       last_component = key_components.pop
       key_components.each do |component|
-        puts "c (#{component})"
+        # puts"c (#{component})"
         level[component] ||= {}
         level = level[component]
       end
